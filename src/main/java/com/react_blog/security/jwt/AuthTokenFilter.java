1ef4839309f3d521aba844c5c3fe81e1b5cc8760
Nicholas Juncos <nicholasjuncos@nicholass-mbp.attlocal.net>
2020-03-30 17:23:14 -0500

    Made correction for authorization header.

--- src/main/java/com/react_blog/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/react_blog/security/jwt/AuthTokenFilter.java
@@ -50,6 +50,9 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
+        if (headerAuth == null) {
+            headerAuth = request.getHeader("authorization");
+        }
 
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
             return headerAuth.substring(7, headerAuth.length());
